6de2c0a4c288fc5cd03c076e495e382931b16341
Nathan Lippi <nathan.lippi@gmail.com>
2014-04-02 14:20:38 -0400

    Now the UI is much better able to handle different currencies.
    The js api is also pretty well-organized.  There is an API for the UI, and an API for programmatically doing things.
    
    It keeps track of which currency needs to be acted upon, and will act differently, with depositing and withdrawing, for different classes of currencies.

--- public/main.js
+++ public/main.js
@@ -1,3 +1,285 @@
+/* Simple JavaScript Inheritance
+ * By John Resig http://ejohn.org/
+ * MIT Licensed.
+ *
+ * http://ejohn.org/blog/simple-javascript-inheritance
+ */
+(function(){
+  var initializing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
+
+  // The base Class implementation (does nothing)
+  this.Class = function(){};
+
+  // Create a new Class that inherits from this class
+  Class.extend = function(prop) {
+    var _super = this.prototype;
+
+    // Instantiate a base class (but only create the instance,
+    // don't run the init constructor)
+    initializing = true;
+    var prototype = new this();
+    initializing = false;
+
+    // Copy the properties over onto the new prototype
+    for (var name in prop) {
+      // Check if we're overwriting an existing function
+      prototype[name] = typeof prop[name] == "function" &&
+        typeof _super[name] == "function" && fnTest.test(prop[name]) ?
+        (function(name, fn){
+          return function() {
+            var tmp = this._super;
+
+            // Add a new ._super() method that is the same method
+            // but on the super-class
+            this._super = _super[name];
+
+            // The method only need to be bound temporarily, so we
+            // remove it when we're done executing
+            var ret = fn.apply(this, arguments);
+            this._super = tmp;
+
+            return ret;
+          };
+        })(name, prop[name]) :
+        prop[name];
+    }
+
+    // The dummy class constructor
+    function Class() {
+      // All construction is actually done in the init method
+      if ( !initializing && this.init )
+        this.init.apply(this, arguments);
+    }
+
+    // Populate our constructed prototype object
+    Class.prototype = prototype;
+
+    // Enforce the constructor to be what we expect
+    Class.prototype.constructor = Class;
+
+    // And make this class extendable
+    Class.extend = arguments.callee;
+
+    return Class;
+  };
+})();
+
+/*
+ * CurrencyTracker
+ *
+ */
+var CurrencyTracker = (function() {
+  var self = this;
+
+  self.acceptableTypes = {
+    FAKE:    "fake",
+    BITCOIN: "btc",
+  };
+
+  self.currencyType = self.acceptableTypes.FAKE;
+
+  function setCurrentType(currencyType) {
+    // TODO: Check that currencyType is of an acceptable type
+    self.currencyType = currencyType;
+  }
+
+  function getCurrentType() {
+    return self.currencyType;
+  }
+
+  return {
+    setCurrentType  : setCurrentType,
+    getCurrentType  : getCurrentType,
+    acceptableTypes : acceptableTypes
+  };
+}());
+
+
+/*
+ * CurrencyModifier(s)
+ *
+ */
+var CurrencyModifier = Class.extend({
+  init: function(currencyType, currencyClass) {
+    this.currencyType  = currencyType;
+    this.currencyClass = currencyClass;
+  },
+  bet: function(amount, callback) {
+    // Convert BTC-type to satoshis
+    amount = to_satoshis(amount);
+    socket.emit("bet:current_jackpot",
+      {currencyType: this.currencyType, amount: amount});
+
+    callback(null);
+  },
+  deposit: function(callback) {
+  },
+  withdraw: function(callback) {
+    callback();
+  }
+});
+
+var CurrencyModifierBSCurrency = CurrencyModifier.extend({
+  init: function(currencyType) {
+    this._super(currencyType, "bs");
+  },
+  deposit: function(callback) {
+    var self = this;
+    callback(null,
+      {
+        prompt: true,
+        currencyType: self.currencyType,
+        additional: {
+          callback: function(amount) {
+            socket.emit("user:add_to_balance",
+              {currencyType: self.currencyType, amount: amount});
+          }
+        }
+      });
+  },
+  withdraw: function(callback) {
+    var self = this;
+    callback(null,
+      {
+        prompt: false,
+        currencyType: self.currencyType,
+        additional: { message: "Sorry, fake money cannot be withdrawn."}
+      });
+  }
+});
+
+var CurrencyModifierVirtualCurrency = CurrencyModifier.extend({
+  init: function(currencyType) {
+    this._super(currencyType, "virtual");
+  },
+  deposit: function(callback) {
+    var self = this;
+    callback(null,
+      {
+        prompt       : false,
+        currencyType : self.currencyType,
+        additional   : {addr: "XYZ_123_ABC"}
+      });
+  },
+  withdraw: function(callback) {
+    var self = this;
+
+    callback(null,
+      {
+        prompt: true,
+        currencyType: self.currencyType,
+        additional: {
+          callback: function(addr, amount) {
+            console.log("Sending "+amount+" "+self.currencyType+" to "+addr);
+          }
+        }
+      });
+  }
+});
+
+var CurrencyModifierFAKE = CurrencyModifierBSCurrency.extend({
+  init: function() {
+    this._super("fake");
+  },
+});
+var CurrencyModifierBITCOIN = CurrencyModifierVirtualCurrency.extend({
+  init: function() {
+    this._super("btc");
+  },
+});
+
+
+/*
+ * BitSplit
+ *
+ */
+var Currencies = (function() {
+  var self = this;
+
+  self.currencyTracker   = CurrencyTracker;
+  self.currencyModifiers = (function() {
+    var currencyModifierObjs = {};
+
+    return {
+      add: function(cmObj) {
+        currencyModifierObjs[cmObj.currencyType] = cmObj;
+      },
+      get: function(currencyType) {
+        if(typeof currencyModifierObjs[currencyType] === "object") {
+          return currencyModifierObjs[currencyType];
+        }
+      }
+    };
+  }());
+
+  self.currencyModifiers.add(new CurrencyModifierFAKE());
+  self.currencyModifiers.add(new CurrencyModifierBITCOIN());
+
+  function getCurrentCurrencyModifier() {
+    return self.currencyModifiers.get(self.currencyTracker.getCurrentType());
+  }
+
+  function bet(amount, callback) {
+    getCurrentCurrencyModifier().bet(amount, callback);
+  }
+  function deposit(callback) {
+    getCurrentCurrencyModifier().deposit(callback);
+  }
+  function withdraw(callback) {
+    getCurrentCurrencyModifier().withdraw(callback);
+  }
+
+  return {
+    setType  : self.currencyTracker.setCurrentType,
+    getType  : self.currencyTracker.getCurrentType,
+    bet      : bet,
+    deposit  : deposit,
+    withdraw : withdraw
+  };
+}());
+
+var currencyUI = (function(Currencies) {
+  var self = this;
+  self.Currencies = Currencies;
+
+  function deposit() {
+    self.Currencies.deposit(function(err, res) {
+      if(res.prompt) {
+        var amount =
+          window.prompt("How many (fake) bitcoins will you add to your balance?", 0);
+        res.additional.callback(to_satoshis(amount));
+      }
+      else {
+        alert("Your Bitcoin deposit address is: "+res.additional.addr);
+      }
+    });
+  }
+
+  function withdraw() {
+    self.Currencies.withdraw(function(err, res) {
+      if(res.prompt) {
+        var addr = window.prompt("Which "+res.currencyType+" address would you like to withdraw to?");
+        var amount = window.prompt("Which "+res.currencyType+" amount?");
+        return res.additional.callback(addr, amount);
+      }
+      return alert(res.additional.message);
+    });
+  }
+
+  // bet, deposit, withdraw (get and set Type?)
+  return {
+    deposit  : deposit,
+    withdraw : withdraw
+  };
+})(Currencies);
+
+var BitSplit = {
+  currency : Currencies,
+  UI       : {
+    currency: currencyUI
+  }
+};
+
 bootstrap_alert = {};
 bootstrap_alert.warning = function(message, fadeout_ms) {
   if(typeof fadeout_ms !== "number") {
@@ -137,8 +419,21 @@ function refresh_past_winners(past_winners)
     }
 }
 
-function place_bet(satoshis) {
-  socket.emit("bet:current_jackpot", satoshis);
+function to_btc(satoshis) {
+    return satoshis/Math.pow(10,8);
+}
+function to_satoshis(btc) {
+    return btc*Math.pow(10,8);
+}
+
+function change_currency_type(currencyType) {
+  socket.emit("user:change_currency_type", currencyType);
+  // TODO: Wait for callback
+  change_currency_type_ui(personal_stats.currencyType);
+}
+function change_currency_type_ui(currencyType) {
+  $("select#currency_type").val(currencyType);
+  BitSplit.currency.setType(currencyType);
 }
 
 function refresh_config(config_data) {
@@ -147,24 +442,24 @@ function refresh_config(config_data) {
         $(".split-time-minutes").html(config_data.split_n_minutes);
     }
 }
-
 function update_personal_stats(personal_stats) {
     $("#user_balance").html(to_btc(personal_stats.balance));
+    change_currency_type_ui(personal_stats.currencyType);
 }
 
 var user = {
   create: function(user_name) {
     socket.emit("user:create", user_name);
   },
-  add_to_balance: function(satoshis) {
-    socket.emit("user:add_to_balance", satoshis);
-  }
 };
 
 $("#deposit").on("click", function(e) {
   e.preventDefault();
-  var amt = window.prompt("How many Bitcoins will you add to your balance?", 0);
-  user.add_to_balance(to_satoshis(amt));
+  BitSplit.UI.currency.deposit();
+});
+$("#withdraw").on("click", function(e) {
+  e.preventDefault();
+  BitSplit.UI.currency.withdraw();
 });
 
 function seconds_to_pretty_time(time_seconds) {
@@ -176,15 +471,7 @@ function seconds_to_pretty_time(time_seconds) {
     }
     return zeroPad(m, 2)+":"+zeroPad(s, 2);
 }
-
 function zeroPad(num, places) {
   var zero = places - num.toString().length + 1;
   return new Array(+(zero > 0 && zero)).join("0") + num;
-}
-
-function to_btc(satoshis) {
-    return satoshis/Math.pow(10,8);
-}
-function to_satoshis(btc) {
-    return btc*Math.pow(10,8);
 }
